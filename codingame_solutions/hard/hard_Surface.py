3295270118d9881acee332988fc8034af8dd37d7
Michal Fularz <michal.fularz@gmail.com>
2015-10-27 18:52:49 +0100

    Moved to serial implementation. What is strange is that using default (float) array to store map of visited cells is faster than int8...

--- codingame_solutions/hard/hard_Surface.py
+++ codingame_solutions/hard/hard_Surface.py
@@ -1,7 +1,11 @@
 __author__ = 'Amin'
 
+# COMPLETED
+# PYTHON 3.x
+
 import sys
 import math
+import numpy as np
 
 
 class Map:
@@ -10,31 +14,31 @@ class Map:
         self.height = height
 
         self.graphical_representation = []
-        self.visited = []
+        self.visited = np.zeros((self.height, self.width))
 
         self.__init_from_input()
 
     def __init_from_input(self):
         for i in range(self.height):
             row = []
-            row_visited = []
             for c in input():
                 row.append(c)
-                row_visited.append(False)
 
             self.graphical_representation.append(row)
-            self.visited.append(row_visited)
 
     def __get_element(self, x, y):
         return self.graphical_representation[y][x]
 
     def __set_as_visited(self, x, y):
-        self.visited[y][x] = True
+        self.visited[y][x] = 1
 
     def __is_visited(self, x, y):
-        return self.visited[y][x]
+        if self.visited[y][x] == 1:
+            return True
+        else:
+            return False
 
-    def check_element(self, x, y):
+    def check_element_recursive(self, x, y):
         lake_size = 0
 
         if 0 <= x < self.width and 0 <= y < self.height:
@@ -45,17 +49,36 @@ class Map:
                 # check elements around
                 lake_size += self.check_element(x, y-1)
                 lake_size += self.check_element(x-1, y)
-                lake_size += self.check_element(x, y)
                 lake_size += self.check_element(x+1, y)
                 lake_size += self.check_element(x, y+1)
 
         return lake_size
 
+    def check_element(self, x, y):
+        lake_size = 0
+
+        elements_to_check = [(x, y)]
+
+        while len(elements_to_check) > 0:
+            element_to_check = elements_to_check.pop(0)
+            x_to_check = element_to_check[0]
+            y_to_check = element_to_check[1]
+            # check first element from the list
+            if 0 <= x_to_check < self.width and 0 <= y_to_check < self.height:
+                if self.__get_element(x_to_check, y_to_check) == "O" and not self.__is_visited(x_to_check, y_to_check):
+                    lake_size += 1
+                    # set this cell as visited
+                    self.__set_as_visited(x_to_check, y_to_check)
+                    # add elements around to be checked
+                    elements_to_check.append((x_to_check, y_to_check-1))
+                    elements_to_check.append((x_to_check-1, y_to_check))
+                    elements_to_check.append((x_to_check+1, y_to_check))
+                    elements_to_check.append((x_to_check, y_to_check+1))
+
+        return lake_size
 
     def clear_visited(self):
-        for i in range(self.height):
-            for j in range(self.width):
-                self.visited[i][j] = False
+        self.visited = np.zeros((self.height, self.width))
 
     def set_visited(self):
         for i in range(self.height):
@@ -78,10 +101,7 @@ class Map:
 
         for row in self.visited:
             for cell in row:
-                if cell:
-                    r += "1"
-                else:
-                    r += "0"
+                r += str(cell)
             r += "\n"
 
         return r
@@ -99,7 +119,6 @@ for i in range(n):
     x, y = [int(j) for j in input().split()]
     lake_size = m.check_element(x, y)
     m.clear_visited()
-    print("Lake size: " + str(lake_size), file=sys.stderr)
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr)
