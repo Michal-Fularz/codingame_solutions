0ec366bbe80a48db0ae93f772eae4b53867756f3
Michał Fularz <michal.fularz@gmail.com>
2015-10-27 15:59:57 +0100

    Just started hard Surface. Looks to be easy :)

---
+++ codingame_solutions/hard/hard_Surface.py
@@ -0,0 +1,21 @@
+__author__ = 'Amin'
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+l = int(input())
+h = int(input())
+for i in range(h):
+    row = input()
+n = int(input())
+for i in range(n):
+    x, y = [int(j) for j in input().split()]
+for i in range(n):
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+
+    print("answer")
