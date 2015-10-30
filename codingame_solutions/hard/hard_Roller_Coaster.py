ca7daf8624486b6b41334bc6a785c7b29b9d3b47
Michał Fularz <michal.fularz@gmail.com>
2015-10-30 08:44:19 +0100

    Fixed error in calculations, yet it is still to slow to pass the performance test.

--- codingame_solutions/hard/hard_Roller_Coaster.py
+++ codingame_solutions/hard/hard_Roller_Coaster.py
@@ -3,6 +3,10 @@ __author__ = 'Amin'
 import sys
 import math
 
+class RingBuffer:
+
+
+
 number_of_places, number_of_rides_per_day, number_of_groups = [int(i) for i in input().split()]
 groups = []
 for i in range(number_of_groups):
@@ -20,7 +24,7 @@ rides_taken = 0
 places_taken = 0
 current_group_size = 0
 flag_take_next_group = True
-flag_group_used = False
+flag_group_used = True
 flag_loop_found = False
 groups_on_the_rollercoaster = []
 number_of_groups_served = 0
@@ -28,7 +32,7 @@ while rides_taken < number_of_rides_per_day and not flag_loop_found:
     while flag_take_next_group and len(groups) > 0:
         if flag_group_used:
             current_group_size = groups.pop(0)
-        if places_taken + current_group_size <= number_of_places:
+        if (places_taken + current_group_size) <= number_of_places:
             places_taken += current_group_size
             groups_on_the_rollercoaster.append(current_group_size)
             number_of_groups_served += 1
@@ -36,52 +40,59 @@ while rides_taken < number_of_rides_per_day and not flag_loop_found:
         else:
             flag_take_next_group = False
             flag_group_used = False
-    #print("Ride taken! Number of people taken: " + str(places_taken), file=sys.stderr)
     rides_taken += 1
     earnings += places_taken
+    #print("Ride taken (nr: " + str(rides_taken) + ")! Number of people taken: " + str(places_taken), file=sys.stderr)
+    #print("Earnings: " + str(earnings) + ", groups served: " + str(number_of_groups_served), file=sys.stderr)
+
     places_taken = 0
     groups += groups_on_the_rollercoaster
     groups_on_the_rollercoaster.clear()
     flag_take_next_group = True
 
     if number_of_groups_served % number_of_groups == 0:
-        print("Groups served: " + str(number_of_groups_served), file=sys.stderr)
         flag_loop_found = True
 
 if flag_loop_found:
     print("Loop found! Rides taken: " + str(rides_taken), file=sys.stderr)
+    print("Groups served: " + str(number_of_groups_served), file=sys.stderr)
     print("Rides per day: " + str(number_of_rides_per_day), file=sys.stderr)
     earnings = earnings * number_of_rides_per_day // rides_taken
     number_of_rides_per_day = number_of_rides_per_day % rides_taken
-
-
     print("Rides per day left: " + str(number_of_rides_per_day), file=sys.stderr)
-    rides_taken = 0
-    places_taken = 0
-    current_group_size = 0
-    flag_take_next_group = True
-    flag_group_used = False
-    flag_loop_found = False
-    groups_on_the_rollercoaster = []
-    number_of_groups_served = 0
-    while rides_taken < number_of_rides_per_day:
-        while flag_take_next_group and len(groups) > 0:
-            if flag_group_used:
-                current_group_size = groups.pop(0)
-            if places_taken + current_group_size <= number_of_places:
-                places_taken += current_group_size
-                groups_on_the_rollercoaster.append(current_group_size)
-                number_of_groups_served += 1
-                flag_group_used = True
-            else:
-                flag_take_next_group = False
-                flag_group_used = False
-        #print("Ride taken! Number of people taken: " + str(places_taken), file=sys.stderr)
-        rides_taken += 1
-        earnings += places_taken
-        places_taken = 0
-        groups += groups_on_the_rollercoaster
-        groups_on_the_rollercoaster.clear()
+
+    # rides_taken = 0
+    # places_taken = 0
+    # current_group_size = 0
+    # flag_take_next_group = True
+    # flag_group_used = True
+    # flag_loop_found = False
+    # groups_on_the_rollercoaster = []
+    # number_of_groups_served = 0
+    # while rides_taken < number_of_rides_per_day and not flag_loop_found:
+    #     while flag_take_next_group and len(groups) > 0:
+    #         if flag_group_used:
+    #             current_group_size = groups.pop(0)
+    #         if (places_taken + current_group_size) <= number_of_places:
+    #             places_taken += current_group_size
+    #             groups_on_the_rollercoaster.append(current_group_size)
+    #             number_of_groups_served += 1
+    #             flag_group_used = True
+    #         else:
+    #             flag_take_next_group = False
+    #             flag_group_used = False
+    #     rides_taken += 1
+    #     earnings += places_taken
+    #     #print("Ride taken (nr: " + str(rides_taken) + ")! Number of people taken: " + str(places_taken), file=sys.stderr)
+    #     #print("Earnings: " + str(earnings) + ", groups served: " + str(number_of_groups_served), file=sys.stderr)
+    #
+    #     places_taken = 0
+    #     groups += groups_on_the_rollercoaster
+    #     groups_on_the_rollercoaster.clear()
+    #     flag_take_next_group = True
+    #
+    #     if number_of_groups_served % number_of_groups == 0:
+    #         flag_loop_found = True
 
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr)
