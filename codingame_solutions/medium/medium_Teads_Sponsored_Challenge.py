596d12d62140626ac336cfec9700b18b70843b88
Michal Fularz <michal.fularz@gmail.com>
2015-11-05 21:31:21 +0100

    Version that uses my graph library. Unfortunately it is too slow.

--- codingame_solutions/medium/medium_Teads_Sponsored_Challenge.py
+++ codingame_solutions/medium/medium_Teads_Sponsored_Challenge.py
@@ -1,157 +1,77 @@
-__author__ = 'Amin'
-
 import sys
 import math
 
-
-class Person:
-    def __init__(self, name):
-        self.neighbours = []
-        self.__number_of_visits = 0
-        self.__name = name
-
-    def add_neighbour(self, new_neighbour):
-        self.neighbours.append(new_neighbour)
-
-    def should_be_visited(self):
-        if self.__number_of_visits < len(self.neighbours):
-            return True
-        else:
-            return False
-            
-    def add_visit(self):
-        self.__number_of_visits += 1
-        
-    def clear_visits(self):
-        self.__number_of_visits = 0
-
-    def get_name(self):
-        return self.__name
-
-
-def propagate(current_person, persons, existing_nodes, recurse=True):
-    depth = 1
-    visited_nodes = [current_person.get_name()]
-    current_person.add_visit()
-
-    depths = []
-    lst_visited_nodes = []
-
-    for neighbour in current_person.neighbours:
-        if persons[neighbour].should_be_visited():
-            d, vn = propagate(persons[neighbour], persons, existing_nodes)
-            depths.append(d)
-            lst_visited_nodes.append(vn)
-
-    if recurse:
-        if len(depths) > 0:
-            max_depth = -1
-            max_depth_index = -1
-            for i in xrange(len(depths)):
-                if depths[i] > max_depth:
-                    max_depth = depths[i]
-                    max_depth_index = i
-
-            depth += depths[max_depth_index]
-            visited_nodes += lst_visited_nodes[max_depth_index]
-
-        return depth, visited_nodes
-
-    else:
-        return depths, lst_visited_nodes
-
-    #if len(depths) > 0:
-        #depth += max(depths)
-
+import numpy as np
+from codingame_solutions.utilities.graph import Graph
 
 
 # MAIN
 
-persons = []
-existing_nodes = []
-number_of_nodes = 0
+persons = Graph()
 
-n = int(raw_input())    # the number of adjacency relations
-
-for i in xrange(n):
+n = int(input())  # the number of adjacency relations
+for i in range(n):
     # xi: the ID of a person which is adjacent to yi
     # yi: the ID of a person which is adjacent to xi
-    xi, yi = [int(j) for j in raw_input().split()]
+    xi, yi = [int(j) for j in input().split()]
 
-    # check if any of the persons is in the set
+    persons.add_edge((xi, yi))
+    persons.add_edge((yi, xi))
 
-    if xi not in existing_nodes:
-        existing_nodes.append(xi)
-        persons.append(Person(existing_nodes.index(xi)))
-        number_of_nodes += 1
+print(persons, file=sys.stderr)
 
-    if yi not in existing_nodes:
-        existing_nodes.append(yi)
-        persons.append(Person(existing_nodes.index(yi)))
-        number_of_nodes += 1
+number_of_vertices = len(persons.vertices())
+print("number_of_vertices: " + str(number_of_vertices), file=sys.stderr)
+neighbourhood = np.zeros((number_of_vertices, number_of_vertices), dtype=int)
+print("neighbourhood: \n" + str(neighbourhood), file=sys.stderr)
+# TODO: conversion between edge name and index is necessary
+for edge in persons.edges():
+    print("edge: " + str(edge), file=sys.stderr)
+    v = list(edge)
+    existing_element_index = v[0]
+    new_element_index = v[1]
+    neighbourhood[existing_element_index][new_element_index] = 1
+    neighbourhood[new_element_index][existing_element_index] = 1
+    
+    # fill the new column
+    for i in range(new_element_index):
+        if i != existing_element_index:
+            neighbourhood[i][new_element_index] = neighbourhood[i][existing_element_index] + 1
+    
+    # fill the new row
+    for j in range(new_element_index):
+        if j != existing_element_index:
+            neighbourhood[new_element_index][j] = neighbourhood[existing_element_index][j] + 1
 
-    persons[existing_nodes.index(xi)].add_neighbour(existing_nodes.index(yi))
-    persons[existing_nodes.index(yi)].add_neighbour(existing_nodes.index(xi))
-
-for person in persons:
-    n = ""
-    for neigh in person.neighbours:
-        n += str(neigh) + ", "
-    print >> sys.stderr, str(person.get_name()) + ", neigh: " + n
-
-flag_continue = True
+print("neighbourhood: \n" + str(neighbourhood), file=sys.stderr)
+    
 
 # lets start with first person that has one neighbour
-depths = []
-lst_visited_nodes = []
-person_with_one_neighbour = persons[0]
-for i in xrange(len(persons)):
-    if len(persons[i].neighbours) == 1:
-        print >> sys.stderr, "index: " + str(i)
-        person_with_one_neighbour = persons[i]
-        break
-    
-depths, lst_visited_nodes = propagate(person_with_one_neighbour, persons, existing_nodes, recurse=False)
-for person in persons:
-    person.clear_visits()
+persons_with_1_neighbour = persons.get_vertices_with_n_edges(1)
+print("persons_with_1_neighbour: " + str(persons_with_1_neighbour), file=sys.stderr)
 
-print >> sys.stderr, "depths: " + str(depths)
-print >> sys.stderr, "lst_visited_nodes: " + str(lst_visited_nodes)
+paths = persons.find_all_paths_from_vertex(persons_with_1_neighbour[0])
+print("paths: " + str(paths), file=sys.stderr)
 
-longest_list_length = len(lst_visited_nodes[0])
-longest_list_index = 0
-for index, lst in enumerate(lst_visited_nodes[1:]):
-    if len(lst) > longest_list_length:
-        longest_list_length = len(lst)
-        longest_list_index = index
+longest_path_length = max([len(l) for l in paths])
 
-print >> sys.stderr, "longest_list_length: " + str(longest_list_length)
-print >> sys.stderr, "longest_list_index: " + str(longest_list_index)
+longest_paths = []
+for path in paths:
+    if len(path) == longest_path_length:
+        longest_paths.append(path)
+print("longest_paths: " + str(longest_paths), file=sys.stderr)
 
-index_of_person_in_the_middle = longest_list_length // 2
-print >> sys.stderr, "index_of_person_in_the_middle: " + str(index_of_person_in_the_middle)
+minimal_distance = 9999
+for path in longest_paths:
+    for person_name in path:
+        paths = persons.find_all_paths_from_vertex(person_name)
+        longest_path_length = max([len(l) for l in paths])
+        print("longest_path_length: " + str(longest_path_length), file=sys.stderr)
+        if longest_path_length < minimal_distance:
+            minimal_distance = longest_path_length
 
-person_in_the_middle = persons[0]
-# find element which name == lst_visited_nodes[avg_item]
-for i in xrange(len(persons)):
-    if persons[i].get_name() == lst_visited_nodes[longest_list_index][index_of_person_in_the_middle]:
-        person_in_the_middle = persons[i]
-        break
-
-print >> sys.stderr, "person_in_the_middle: " + str(person_in_the_middle.get_name())
-
-depths, lst_visited_nodes = propagate(person_in_the_middle, persons, existing_nodes, recurse=False)
-
-print >> sys.stderr, "depths: " + str(depths)
-print >> sys.stderr, "lst_visited_nodes: " + str(lst_visited_nodes)
-
-longest_list_of_visited_nodes = max([len(l) for l in lst_visited_nodes])
-print >> sys.stderr, "longest_list_of_visited_nodes: " + str(longest_list_of_visited_nodes)
-
-longest_list_length = max([len(l) for l in lst_visited_nodes])
-
-# for depth in depths:
-#     print >> sys.stderr, "Depth: " + str(depth)
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
 
 # The minimal amount of steps required to completely propagate the advertisement
-print longest_list_length
+print(minimal_distance-1)
