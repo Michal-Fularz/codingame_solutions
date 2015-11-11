eb1d3c28cfe0a64b0cf468fff09d2cb03bea83d2
Michal Fularz <michal.fularz@gmail.com>
2015-11-11 13:33:44 +0100

    New idea works for all the tests! Unfortunately it does not get special bonus :[

--- codingame_solutions/very_hard/very_hard_Triangulation.py
+++ codingame_solutions/very_hard/very_hard_Triangulation.py
@@ -36,11 +36,25 @@ class Direction(Enum):
             return Direction.down_right
 
 
+class Movements(Enum):
+    vertical = 1
+    horizontal = 2
+    mixed = 3
+
+    @staticmethod
+    def get_opposite(movement):
+        if movement == Movements.vertical:
+            return Movements.horizontal
+        elif movement == Movements.horizontal:
+            Movements.vertical
+        else:
+            return Movements.mixed
+
+
 class Building:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        #print("Width: " + str(self.width) + ", height: " + str(self.height), file=sys.stderr)
 
         self.usable_x_min = 0
         self.usable_x_max = self.width
@@ -59,11 +73,11 @@ class Building:
                 y_start = batman.y_previous + distance_traveled // 2
                 y_end = batman.y_current - distance_traveled // 2 + 1
 
-                self.map[self.usable_y_min:y_start, :] = -1
-                self.usable_y_min = y_start + 1
+                self.map[self.usable_y_min:y_start, self.usable_x_min:self.usable_x_max] = -1
+                self.usable_y_min = y_start
                 #self.map[0:y_start, :] = -1
-                self.map[y_end:self.usable_y_max, :] = -1
-                self.usable_y_max = y_end - 1
+                self.map[y_end:self.usable_y_max, self.usable_x_min:self.usable_x_max] = -1
+                self.usable_y_max = y_end
                 #self.map[y_end:self.height, :] = -1
 
             elif batman.direction_current == Direction.up:
@@ -72,28 +86,25 @@ class Building:
                 y_start = batman.y_current + distance_traveled // 2
                 y_end = batman.y_previous - distance_traveled // 2 + 1
 
-                self.map[self.usable_y_min:y_start, :] = -1
-                self.usable_y_min = y_start + 1
+                self.map[self.usable_y_min:y_start, self.usable_x_min:self.usable_x_max] = -1
+                self.usable_y_min = y_start
                 #self.map[0:y_start, :] = -1
-                self.map[y_end:self.usable_y_max, :] = -1
-                self.usable_y_max = y_end - 1
+                self.map[y_end:self.usable_y_max, self.usable_x_min:self.usable_x_max] = -1
+                self.usable_y_max = y_end
                 #self.map[y_end:self.height, :] = -1
 
-            # this part is done only when the right row is chosen
             elif batman.direction_current == Direction.left:
                 distance_traveled = batman.x_previous - batman.x_current
 
                 x_start = batman.x_current + distance_traveled // 2
                 x_end = batman.x_previous - distance_traveled // 2 + 1
 
-                self.map[batman.y_current, self.usable_x_min:x_start] = -1
-                self.usable_x_min = x_start + 1
+                self.map[self.usable_y_min:self.usable_y_max, self.usable_x_min:x_start] = -1
+                self.usable_x_min = x_start
                 #self.map[batman.y_current, 0:x_start] = -1
-                self.map[batman.y_current, x_end:self.usable_x_max] = -1
-                self.usable_x_max = x_end - 1
+                self.map[self.usable_y_min:self.usable_y_max, x_end:self.usable_x_max] = -1
+                self.usable_x_max = x_end
                 #self.map[batman.y_current, x_end:self.width] = -1
-
-                #print(self.map[batman.y_current], file=sys.stderr)
 
             elif batman.direction_current == Direction.right:
                 distance_traveled = batman.x_current - batman.x_previous
@@ -101,16 +112,16 @@ class Building:
                 x_start = batman.x_previous + distance_traveled // 2
                 x_end = batman.x_current - distance_traveled // 2 + 1
 
-                self.map[batman.y_current, self.usable_x_min:x_start] = -1
-                self.usable_x_min = x_start + 1
+                self.map[self.usable_y_min:self.usable_y_max, self.usable_x_min:x_start] = -1
+                self.usable_x_min = x_start
                 #self.map[batman.y_current, 0:x_start] = -1
-                self.map[batman.y_current, x_end:self.usable_x_max] = -1
-                self.usable_x_max = x_end - 1
+                self.map[self.usable_y_min:self.usable_y_max, x_end:self.usable_x_max] = -1
+                self.usable_x_max = x_end
                 #self.map[batman.y_current, x_end:self.width] = -1
 
-                #print(self.map[batman.y_current], file=sys.stderr)
-
             else:
+                # TODO: this is special case and can be treated accordingly
+                # if last direction is one of up_right, up_left, down_right, down_left do not update the map
                 pass
 
         elif bomb_dist == "WARMER":
@@ -119,8 +130,8 @@ class Building:
 
                 y_start = batman.y_previous + distance_traveled // 2 + 1
 
-                self.map[self.usable_y_min:y_start, :] = -1
-                self.usable_y_min = y_start + 1
+                self.map[self.usable_y_min:y_start, self.usable_x_min:self.usable_x_max] = -1
+                self.usable_y_min = y_start
                 #self.map[0:y_start, :] = -1
 
             elif batman.direction_current == Direction.up:
@@ -128,43 +139,30 @@ class Building:
 
                 y_end = batman.y_previous - distance_traveled // 2
 
-                self.map[y_end:self.usable_y_max, :] = -1
-                self.usable_y_max = y_end - 1
+                self.map[y_end:self.usable_y_max, self.usable_x_min:self.usable_x_max] = -1
+                self.usable_y_max = y_end
                 #self.map[y_end:self.height, :] = -1
 
-            # this part is done only when the right row is chosen
             elif batman.direction_current == Direction.left:
                 distance_traveled = batman.x_previous - batman.x_current
 
                 x_end = batman.x_previous - distance_traveled // 2
 
-                self.map[batman.y_current, x_end:self.usable_x_max] = -1
-                self.usable_x_max = x_end - 1
+                self.map[self.usable_y_min:self.usable_y_max, x_end:self.usable_x_max] = -1
+                self.usable_x_max = x_end
                 #self.map[batman.y_current, x_end:self.width] = -1
-
-                #print(self.map[batman.y_current], file=sys.stderr)
 
             elif batman.direction_current == Direction.right:
                 distance_traveled = batman.x_current - batman.x_previous
 
                 x_start = batman.x_previous + distance_traveled // 2 + 1
 
-                self.map[batman.y_current, self.usable_x_min:x_start] = -1
-                self.usable_x_min = x_start + 1
+                self.map[self.usable_y_min:self.usable_y_max, self.usable_x_min:x_start] = -1
+                self.usable_x_min = x_start
                 #self.map[batman.y_current, 0:x_start] = -1
 
-                #print(self.map[batman.y_current], file=sys.stderr)
-
-            elif batman.direction_current == Direction.up_right:
-                pass
-
-            elif batman.direction_current == Direction.up_left:
-                pass
-
-            elif batman.direction_current == Direction.down_right:
-                pass
-
-            elif batman.direction_current == Direction.down_left:
+            else:
+                # if last direction is one of up_right, up_left, down_right, down_left do not update the map
                 pass
 
         elif bomb_distance == "COLDER":
@@ -173,8 +171,8 @@ class Building:
 
                 y_end = batman.y_current - distance_traveled // 2
 
-                self.map[y_end:self.usable_y_max, :] = -1
-                self.usable_y_max = y_end - 1
+                self.map[y_end:self.usable_y_max, self.usable_x_min:self.usable_x_max] = -1
+                self.usable_y_max = y_end
                 #self.map[y_end:self.height, :] = -1
 
             elif batman.direction_current == Direction.up:
@@ -182,30 +180,33 @@ class Building:
 
                 y_start = batman.y_current + distance_traveled // 2 + 1
 
-                self.map[self.usable_y_min:y_start, :] = -1
-                self.usable_y_min = y_start + 1
+                self.map[self.usable_y_min:y_start, self.usable_x_min:self.usable_x_max] = -1
+                self.usable_y_min = y_start
                 #self.map[0:y_start, :] = -1
 
-            # this part is done only when the right row is chosen
             elif batman.direction_current == Direction.left:
                 distance_traveled = batman.x_previous - batman.x_current
 
                 x_start = batman.x_current + distance_traveled // 2 + 1
 
-                self.map[batman.y_current, self.usable_x_min:x_start] = -1
-                self.usable_x_min = x_start + 1
+                self.map[self.usable_y_min:self.usable_y_max, self.usable_x_min:x_start] = -1
+                self.usable_x_min = x_start
                 #self.map[batman.y_current, 0:x_start] = -1
-                #print(self.map[batman.y_current], file=sys.stderr)
 
             elif batman.direction_current == Direction.right:
                 distance_traveled = batman.x_current - batman.x_previous
 
                 x_end = batman.x_current - distance_traveled // 2
 
-                self.map[batman.y_current, x_end:self.usable_x_max] = -1
-                self.usable_x_max = x_end - 1
+                self.map[self.usable_y_min:self.usable_y_max, x_end:self.usable_x_max] = -1
+                self.usable_x_max = x_end
                 #self.map[batman.y_current, x_end:self.width] = -1
-                #print(self.map[batman.y_current], file=sys.stderr)
+
+            else:
+                # if last direction is one of up_right, up_left, down_right, down_left do not update the map
+                pass
+        else:   # UNKNOWN
+            pass
 
         #print(self.map, file=sys.stderr)
         print("self.usable_x_min: " + str(self.usable_x_min), file=sys.stderr)
@@ -390,6 +391,65 @@ class Building:
 
         return direction, new_position
 
+    def check_if_only_one_column_left(self):
+        usable_x_width = self.usable_x_max - self.usable_x_min
+
+        if usable_x_width == 1:
+            return True
+        else:
+            return False
+
+    def check_if_only_one_row_left(self):
+        usable_y_width = self.usable_y_max - self.usable_y_min
+
+        if usable_y_width == 1:
+            return True
+        else:
+            return False
+
+    def check_if_only_one_cell_left(self):
+        usable_x_width = self.usable_x_max - self.usable_x_min
+        usable_y_width = self.usable_y_max - self.usable_y_min
+
+        if usable_x_width == 1 and usable_y_width == 1:
+            return True
+        else:
+            return False
+
+    def find_next_position_new(self, current_x, current_y, movement):
+
+        usable_y_midpoint = (self.usable_y_max + self.usable_y_min) // 2
+        usable_x_midpoint = (self.usable_x_max + self.usable_x_min) // 2
+
+        print("Midpoint x: " + str(usable_x_midpoint) + ", y: " + str(usable_y_midpoint), file=sys.stderr)
+
+        if movement == Movements.vertical:
+            if current_y < usable_y_midpoint:
+                if current_y < self.usable_y_min:
+                    new_y = self.usable_y_min
+                else:
+                    new_y = (self.usable_y_max - 1) - (current_y - self.usable_y_min)
+            else:
+                if current_y > self.usable_y_max:
+                    new_y = self.usable_y_max - 1
+                else:
+                    new_y = max(self.usable_y_min + ((self.usable_y_max - 1) - current_y), self.usable_y_min)
+
+            print("New y: " + str(new_y), file=sys.stderr)
+
+            return current_x, new_y
+        elif movement == Movements.horizontal:
+            if current_x < usable_x_midpoint:
+                new_x = (self.usable_x_max - 1) - (current_x - self.usable_x_min)
+            else:
+                new_x = self.usable_x_min + ((self.usable_x_max - 1) - current_x)
+
+            print("New x: " + str(new_x), file=sys.stderr)
+
+            return new_x, current_y
+        else:
+            return self.usable_x_max-1, self.usable_y_max-1
+
     def find_next_position(self, current_x, current_y, direction):
 
         if direction != Direction.up_right and direction != Direction.up_left and \
@@ -453,17 +513,39 @@ class Building:
 
 class Batman:
     def __init__(self, x0, y0):
-        self.x_initial = x0
-        self.y_initial = y0
+        self.x_current = x0
+        self.y_current = y0
 
-        self.x_current = self.x_initial
-        self.y_current = self.y_initial
+        self.x_previous = x0
+        self.y_previous = y0
 
-        self.x_previous = self.x_initial
-        self.y_previous = self.y_initial
+        self.movement = Movements.vertical
 
         self.direction_current = Direction.up
         self.direction_previous = Direction.up
+
+        self.vertical_distance = "UNKNOWN"
+        self.horizontal_distance = "UNKNOWN"
+
+    def update_movement(self, current_distance):
+
+        if current_distance != "UNKNOWN":
+            if self.movement == Movements.vertical:
+                self.vertical_distance = current_distance
+                self.movement = Movements.horizontal
+            elif self.movement == Movements.horizontal:
+                self.horizontal_distance = current_distance
+                if self.vertical_distance == "WARMER" and self.horizontal_distance == "WARMER":
+                    self.movement = Movements.vertical
+                else:
+                    self.movement = Movements.mixed
+            else:   # UNKNOWN
+                self.movement = Movements.vertical
+        else:
+            pass
+
+    def set_movement(self, new_movement):
+        self.movement = new_movement
 
     def set_position(self, x, y):
         self.x_previous = self.x_current
@@ -472,12 +554,25 @@ class Batman:
         self.x_current = x
         self.y_current = y
 
-    def update(self, dx, dy):
-        self.x_previous = self.x_current
-        self.y_previous = self.y_current
+        self.__update_direction()
 
-        self.x_current += dx
-        self.y_current -= dy
+    def __update_direction(self):
+        # this could be decided based on self. movement, but this method is more general
+        if self.x_current == self.x_previous:
+            # vertical
+            if self.y_current < self.y_previous:
+                self.direction_current = Direction.up
+            else:
+                self.direction_current = Direction.down
+        elif self.y_current == self.y_previous:
+            # horizontal
+            if self.x_current < self.x_previous:
+                self.direction_current = Direction.left
+            else:
+                self.direction_current = Direction.right
+        else:
+            # one of mixed direction, does not matter which
+            self.direction_current = Direction.up_right
 
     def update_based_on_direction2(self, direction, new_position_main, new_position_additional):
         self.x_previous = self.x_current
@@ -501,26 +596,13 @@ class Batman:
         else:
             print("ERROR!!! Batman can't update in that direction!", file=sys.stderr)
 
-    def get_new_position_based_on_direction(self, direction, distance):
-        new_x = self.x_current
-        new_y = self.y_current
-
-        if direction == Direction.up:
-            new_y -= distance
-        elif direction == Direction.down:
-            new_y -= -distance
-        elif direction == Direction.right:
-            new_x += distance
-        elif direction == Direction.left:
-            new_x += -distance
-
-        return new_x, new_y
-
     def get_as_string(self):
-        r = "Batman position: \n"
+        r = "Batman: \n"
         r += "x: " + str(self.x_current) + ", y: " + str(self.y_current) + "\n"
         r += "x_p: " + str(self.x_previous) + ", y_p: " + str(self.y_previous) + "\n"
-        r += "dir: " + str(self.direction_current) + ", dir_p: " + str(self.direction_previous)
+        r += "dir: " + str(self.direction_current) + ", dir_p: " + str(self.direction_previous) + "\n"
+        r += "dis_vertical: " + str(self.vertical_distance) + ", dis_horizontal: " + str(self.horizontal_distance) + "\n"
+        r += "movement: " + str(self.movement)
 
         return r
 
@@ -529,52 +611,37 @@ if __name__ == '__main__':
     # w: width of the building.
     # h: height of the building.
     w, h = [int(i) for i in input().split()]
+
+    print("w: " + str(w) + ", h: " + str(h), file=sys.stderr)
+
     n = int(input())  # maximum number of turns before game over.
     x0, y0 = [int(i) for i in input().split()]
 
     building = Building(w, h)
     batman = Batman(x0, y0)
 
-    # IDEAS:
-    # use abs to calculate distance instead of power and square root - maybe it will be faster
-    # THOSE TWO METRICS GIVES DIFFERENT VALUES
-    # b = np.zeros((5, 5))
-    # for i in range(5):
-    #     for j in range(5):
-    #         b[i][j] = math.sqrt(pow(i-3, 2)+pow(j-4, 2))
-    # print(b)
-    #
-    # a = np.zeros((5, 5))
-    # for i in range(5):
-    #     for j in range(5):
-    #         a[i][j] = abs(i-3) + abs(j-4)
-    # print(a)
-
-    bomb_dist = input()  # Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
-
-    # first round is special - bomb_distance = UNKNOWN
-    current_direction, new_pos = building.find_movements_first_round(batman.x_current, batman.y_current)
-
-    batman.update_based_on_direction2(current_direction, new_pos)
-    batman.direction_previous = current_direction
-
-    print(str(batman.x_current) + " " + str(batman.y_current))
-
     # game loop
     while 1:
-        previous_bomb_dist = bomb_dist
         bomb_dist = input()  # Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
+
+        batman.update_movement(bomb_dist)
+
+        print(batman.get_as_string(), file=sys.stderr)
 
         building.update_map(batman, bomb_dist)
 
-        print(building.map, file=sys.stderr)
+        if building.check_if_only_one_cell_left():
+            batman.set_position(building.usable_x_min, building.usable_y_min)
+        else:
+            if building.check_if_only_one_column_left():
+                batman.set_movement(Movements.vertical)
+            if building.check_if_only_one_row_left():
+                batman.set_movement(Movements.horizontal)
 
-        current_direction = building.find_movements_based_on_distance(batman, bomb_dist)
-        print("Direction choosen: " + str(current_direction), file=sys.stderr)
-        new_pos_main, new_pos_additional = building.find_next_position(batman.x_current, batman.y_current, current_direction)
-        print("New position: " + str(new_pos), file=sys.stderr)
-        batman.update_based_on_direction2(current_direction, new_pos_main, new_pos_additional)
-        print(batman.get_as_string(), file=sys.stderr)
+            #print(building.map, file=sys.stderr)
+
+            batman_new_x, batman_new_y = building.find_next_position_new(batman.x_current, batman.y_current, batman.movement)
+            batman.set_position(batman_new_x, batman_new_y)
 
         # Write an action using print
         # To debug: print("Debug messages...", file=sys.stderr)
